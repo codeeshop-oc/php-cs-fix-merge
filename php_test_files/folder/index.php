2884826ff676319eadb808e8a20011651d8d50f3
codeeshop-oc <anantnegi8@gmail.com>
2023-02-07 19:27:54 +0530

    updated

---
+++ php_test_files/folder/index.php
@@ -0,0 +1,15 @@
+<?php
+
+  echo "dd  ";
+  echo  "here";
+  echo  "here";
+  echo  "here";
+  echo  "here";
+
+
+  $a = array('herer', 'now', 'welcome') ;
+  $b = array('herer',    'now'  ,    'welcome') ;
+  $c = [
+    'herer '   ,
+  'now' ,
+   'welcome'] ;
